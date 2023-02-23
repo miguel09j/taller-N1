1e204e2cfb1738be256211ae4d09eed4737b6c37
miguel09j <110635601+miguel09j@users.noreply.github.com>
2023-02-23 11:33:15 -0500

    Add files via upload
    taller numero 1 (progrmacion enfocada a sistemas mecatronicos)

---
+++ taller de programacion punto 2.py
@@ -0,0 +1,8 @@
+estatura = float(input("ingrese su estatura en centimetros"))
+peso = float(input("ingrese su peso en kg"))
+
+masa = peso/(estatura)^2
+
+print("la masa corpolar es: ", masa)
+
+
---
+++ taller programacion punto 1.py
@@ -0,0 +1,5 @@
+dividiendo = int(input("ingresen el dividiendo de la division"))
+divisor = int(input("ingrese el divisor de la division"))
+
+cociente = print("el cociente es: ", str(dividiendo/divisor))
+residuo = print("el residuo es: ", dividiendo % divisor)
